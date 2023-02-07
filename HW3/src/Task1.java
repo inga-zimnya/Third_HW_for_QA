bcc3fa1d0078f65c9f53957c4ec5e93ceef051e8
Inga <engaezik@gmail.com>
2023-02-07 11:42:36 +0300

    Checking of the word "Java"

--- HW3/src/Task1.java
+++ HW3/src/Task1.java
@@ -1,7 +1,13 @@
 public class Task1 {
     public static void main(String[] args) {
         String test = "I like Java and practise it every day!";
+        String sec = "I like Java!!!";
         //check if the string starts with prefix "like"
         System.out.println(test.startsWith("like"));
+
+        //find the position of the word "Java" in the second string
+        System.out.println(sec.indexOf("J"));
+
+
     }
 }
