d074412951fd0139278effd3d9cbcfaec46c73e9
Inga <engaezik@gmail.com>
2023-02-07 11:37:11 +0300

    The first point of the first task

--- HW3/src/Task1.java
+++ HW3/src/Task1.java
@@ -1,5 +1,7 @@
 public class Task1 {
     public static void main(String[] args) {
-
+        String test = "I like Java and practise it every day!";
+        //check if the string starts with prefix "like"
+        System.out.println(test.startsWith("like"));
     }
 }
